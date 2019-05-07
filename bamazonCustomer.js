5ac7b83a25072e0d059e6bfd7270864a67f77af9
Pandapoc <45634681+Pandapoc@users.noreply.github.com>
2019-05-06 20:46:57 -0700

    hide passwords

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,7 @@
 const { createConnection } = require('mysql2')
 const { prompt } = require('inquirer')
 
-const db = createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Nickisawesome1!',
-    database: 'bamazon'
-})
+const db = require('./passwords')
 
 const init = () => {
     db.connect(e => {
